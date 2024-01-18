d062bbb9e6731d18ace5421af3a91ddc268d4a32
Omolayoayokanmi <97984877+Itzbeejay01@users.noreply.github.com>
2024-01-18 13:21:21 +0100

    Add files via upload

---
+++ api/connection.php
@@ -0,0 +1,15 @@
+<?php
+$dbhost = 'db4free.net';
+$dbuser = 'jabucsc';
+$dbpassword = 'Jabucsc123@';
+$dbname = 'jabucsc'; 
+$dbport = 3306;
+
+$connect = mysqli_connect($dbhost, $dbuser, $dbpassword, $dbname, $dbport);
+if (mysqli_connect_errno()) {
+echo "<div style='background-color:red;color:white;text-align:center; padding: 7px'> Database not connected!</div>";
+exit();
+} else {
+   //echo "<div style='background-color:green;color:white;text-align:center; padding: 7px'>Database connected</div>";
+}
+?>
---
+++ api/index.php
@@ -0,0 +1,41 @@
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>JABU</title>
+    <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css" rel="stylesheet">
+    <link rel="shortcut icon" href="logo.php" type="image/x-icon">
+</head>
+
+<body class="w-full h-full bg-gray-100 py-24 p-10 lg:px-96 lg:py-20">
+    <div class="w-full h-full bg-white rounded py-16 p-8 shadow-md lg:w-full lg:flex lg:flex-col lg:py-20 lg:justify-center lg:items-center">
+        <div class="flex flex-col items-center">
+            <h2 class="text-xl text-slate-900 font-bold mb-2 text-center">JOSEPH AYO BABALOLA UNIVERSITY</h2>
+            <h2 class="text-lg text-slate-900 mb-4 text-center">Computer Science Department</h2>
+              <img class="w-24" src="logo.php" alt="">
+            <h2 class="text-lg font-medium text-center text-slate-900 mb-5">300L C.A. SCORE BOARD</h2>
+        </div>
+        <form  method="post" class="w-full h-full pb-3 lg:px-14">
+          <div class="mb-4">
+            <input type="text" id="matric" name="matricNumber" class="border-2 outline-none rounded-md form-input mt-1 p-2 w-full placeholder:text-gray-800"
+                placeholder="Matric Number"  required>
+          </div>
+            <div class="mb-6">
+                <select id="course" name="course" class="text-gray-800 border-2 outline-none rounded-md form-select mt-1 p-2 w-full" required>
+                    <option value="" disabled selected>Select Course</option>
+                    <option value="csc315">CSC 315</option>
+                    <option value="csc317">CSC 317</option>
+                </select>
+            </div>
+            <button name="login" type="submit" class="w-full bg-gray-700 text-white p-2 rounded shadow-lg font-bold text-lg hover:bg-slate-500 active:bg-slate-500">Check Result</button>
+
+        </form>
+    </div>
+</body>
+</html>
+<?php
+include 'login.php';
+?>
